ba32587c5e083d413beb76136fcc27eccf4272c1
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-11 15:58:37 -0300

    Implementando o Padrão de Projetos VO, Adicionando DozerMapper e adicionando os testes unitarios do DozerMapper, Refatorando Service e Controler para usar o DozerMapper

--- rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import restwithspringbootandjavaerudio.model.Person;
+import restwithspringbootandjavaerudio.data.vo.v1.PersonVO;
 import restwithspringbootandjavaerudio.services.PersonServices;
 
 
@@ -24,26 +24,25 @@ public class PersonController {
 
 	@Autowired
 	private PersonServices service;
-	/// private PersonServices service = new PersonServices();
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 
 	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable(value = "id") Long id) {
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
 
 	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
-		return service.create(person);
+	public PersonVO create(@RequestBody PersonVO personVO) {
+		return service.create(personVO);
 	}
 
 	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
-		return service.update(person);
+	public PersonVO update(@RequestBody PersonVO personVO) {
+		return service.update(personVO);
 	}
 
 	@DeleteMapping(value = "/{id}")
