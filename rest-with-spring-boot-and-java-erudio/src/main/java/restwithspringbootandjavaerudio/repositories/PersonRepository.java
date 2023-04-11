5d47c9b63f39a3aa6f78a84a2c092112b1efa23d
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-11 14:26:10 -0300

    Criando Repository e refatorando PersonController e PersonService

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/repositories/PersonRepository.java
@@ -0,0 +1,9 @@
+package restwithspringbootandjavaerudio.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import restwithspringbootandjavaerudio.model.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {}
