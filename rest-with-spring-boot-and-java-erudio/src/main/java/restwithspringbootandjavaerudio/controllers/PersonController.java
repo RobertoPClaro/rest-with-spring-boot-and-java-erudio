bd5c380c04060c7b08446ab07e46c6b086805a5a
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-13 20:24:34 -0300

    Adicionando e configurando o CORS e criando testes

--- rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,7 +24,7 @@ import restwithspringbootandjavaerudio.data.vo.v1.PersonVO;
 import restwithspringbootandjavaerudio.services.PersonServices;
 import restwithspringbootandjavaerudio.util.MediaType;
 
-
+//@CrossOrigin //libera para todos os metodos do controller
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for Managing People")
@@ -54,6 +55,7 @@ public class PersonController {
 			return service.findAll();
 		}
 		
+		//@CrossOrigin(origins = "http://localhost:8080") //libera o cors para apenas o metodoespecifico
 		@GetMapping(value = "/{id}",
 			produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML  })
 		@Operation(summary = "Finds a Person", description = "Finds a Person",
