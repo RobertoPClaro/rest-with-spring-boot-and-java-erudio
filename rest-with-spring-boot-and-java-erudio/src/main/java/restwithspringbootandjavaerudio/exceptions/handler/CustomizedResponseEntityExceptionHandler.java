5a72f39da4d32df0d3d807f75450f9ad83e8ea79
Roberto Pacheco Claro <robertopacheco1994@gmail.com>
2023-04-18 18:35:47 -0300

    Implementando Autenticação com JWT e Spring Security

--- rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/restwithspringbootandjavaerudio/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import restwithspringbootandjavaerudio.exceptions.ExceptionResponse;
+import restwithspringbootandjavaerudio.exceptions.InvalidJwtAuthenticationException;
 import restwithspringbootandjavaerudio.exceptions.RequiredObjectIsNullException;
 import restwithspringbootandjavaerudio.exceptions.ResourceNotFoundException;
 
@@ -51,5 +52,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationExceptions(
+			Exception ex, WebRequest request) {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(),
+				ex.getMessage(),
+				request.getDescription(false));
+		
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
 	}
 }
